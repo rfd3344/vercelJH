597743a4bf63abaeadbb0b6d2747e561a36e515b
rfd3344w <rfd3344w@gmail.com>
2024-12-05 15:58:42 +1100

    1

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,31 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 
-import ServerDemo from './ServerDemo';
-
 import ClientDemo1 from 'src/sections/demo/ClientDemo1';
 import ClientDemo2 from 'src/sections/demo/ClientDemo2';
 
-interface IProp {
-  params: {
-    a?: string;
-  };
-  searchParam: {
-    aa: string;
-  }
-}
+import ServerDemo from './ServerDemo';
 
 
-export default async function Page({ params, searchParam }: IProp) {
-  const { a = [] } = params;
-  console.warn('params', params, searchParam);
+
+export default async function Page() {
+
   return (
-    <main>
+    <main id="demo">
       <ServerDemo />
       <ClientDemo1 />
       <ClientDemo2 />
-
-
     </main>
   );
 }
