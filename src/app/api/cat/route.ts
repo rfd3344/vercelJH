f45a9e90ddbd47dedd4cbb5714ec16ce29677a7e
rfd3344w <rfd3344w@gmail.com>
2024-12-15 22:41:47 +1100

    Update route.ts

--- src/app/api/cat/route.ts
+++ src/app/api/cat/route.ts
@@ -46,19 +46,14 @@ export async function POST(req: NextRequest) {
   return NextResponse.json(resp, { status: 200 });
 }
 
-export async function Patch(req: NextRequest) {
-  const resp = await getDocs('cat').catch(err => {
-    console.error('error', err)
-    return {
-      ...err,
-      error: 'Fail to connect MongoDB',
-    }
-  });
-
-  if (resp.error) {
-    return NextResponse.json(resp, { status: 400 });
-  }
+export async function PATCH(req: NextRequest) {
 
 
-  return NextResponse.json(resp, { status: 200 });
+  return NextResponse.json('resp', { status: 200 });
+}
+
+
+export async function DELETE(req: NextRequest) {
+
+  return NextResponse.json('resp', { status: 200 });
 }
