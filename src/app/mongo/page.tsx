af608ad730f711d53b7eb63c37893aefb43f1964
rfd3344w <rfd3344w@gmail.com>
2024-12-24 17:58:21 +1100

    Update page.tsx

--- src/app/mongo/page.tsx
+++ src/app/mongo/page.tsx
@@ -5,7 +5,6 @@ import MongoActions from 'src/sections/mongo/MongoActions';
 import MongoTable from 'src/sections/mongo/MongoTable';
 import MongoDialog from 'src/sections/mongo/MongoDialog';
 
-import { publicEnv } from 'src/core/envConfig';
 
 import { store } from 'src/core/redux/store';
 import vercelQuery from 'src/query/vercelQuery';
