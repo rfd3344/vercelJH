c3bd6e5db84ab5af96cda12d44a26d9bdc2590bb
rfd3344w <rfd3344w@gmail.com>
2025-01-09 10:40:36 +1100

    1

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -3,25 +3,25 @@ import {
   NextResponse,
 } from 'src/libs/next';
 
-import { openAiChat } from 'src/libs/openai';
+// import { openAiChat } from 'src/libs/openai';
 
 export async function GET(req: NextRequest) {
 
-  const resp = await openAiChat([
-    {
-      role: 'user',
-      content: 'Write a resume for software developer',
-    },
-  ]);
-  console.warn('resp', resp);
-  return NextResponse.json(resp);
+  // const resp = await openAiChat([
+  //   {
+  //     role: 'user',
+  //     content: 'Write a resume for software developer',
+  //   },
+  // ]);
+  // console.warn('resp', resp);
+  return NextResponse.json({ message: 'Hello World' });
 }
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  // const body = await req.json();
 
 
-  const resp = await openAiChat(body);
+  // const resp = await openAiChat(body);
 
-  return NextResponse.json(resp);
+  return NextResponse.json({ message: 'Hello World' });
 }
--- src/app/openai/page.tsx
+++ src/app/openai/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-import ChatCompletions from 'src/sections/openai/ChatCompletions';
+// import ChatCompletions from 'src/sections/openai/ChatCompletions';
 
 
-import { example } from 'src/libs/openai';
+// import { example } from 'src/libs/openai';
 export default async function Page() {
   // const resp =  await example();
   // console.warn(resp);
   return (
     <main id="openai" className='container'>
-      <ChatCompletions />
+      {/* <ChatCompletions /> */}
 
     </main>
   );
