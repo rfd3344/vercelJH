7cf4642dd31d66ea6243f7ac4ddce245a06ab2bc
rfd3344w <rfd3344w@gmail.com>
2025-01-17 11:03:57 +1100

    Update Note.tsx

--- src/sections/supabase/note/Note.tsx
+++ src/sections/supabase/note/Note.tsx
@@ -8,54 +8,56 @@ import { supabase } from 'src/libs/supabase';
 
 export default function Note() {
 
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState<any>({});
 
 
   useEffect(() => {
     supabase.from('note').select().order('id').then((resp: any) => {
-      setNotes(resp.data);
+      const nextNotes = _.keyBy(resp.data, 'id');
+      setNotes(nextNotes);
     });
   }, []);
 
-  const handleChange = (id = 0, content = '') => {
-    // console.warn('id', content, id);
-    supabase.from('note').update({ content }).eq('id', id);
+  const handleBlur = (id = 0, content = '') => {
+    if(notes[id].content === content) return;
+
+    supabase.from('note').update({ content }).eq('id', id).select().then((resp: any) => {
+      const data = resp?.data[0];
+      setNotes({
+        ...notes,
+        [data.id]: data,
+      });
+    });
   };
 
 
   const handleInput = (e: any) => {
     const target = e.target;
-    // console.warn(target.scrollHeight);
+
     if (target.scrollHeight < 500 || target.scrollHeight > 800) {
       return;
     }
+
     target.style.height = `${target.scrollHeight}px`;
   };
 
   return (
-
     <section id="Note" className='mt-2'>
       <div>
         <Tabs variant="fullWidth" className='[&_button]:px-1 gap-0'>
-          {notes.map((item: any) => (
+          {_.values(notes).map((item: any) => (
             <Tabs.Item key={item.id} title={item.title} >
               <Textarea
                 rows={20}
                 defaultValue={item.content}
                 onInput={handleInput}
-                // onChange={_.debounce((e) => handleChange(item.id, e.target.value), 2000)}
-                onBlur={(e) => handleChange(item.id, e.target.value)}
+                onBlur={(e) => handleBlur(item.id, e.target.value)}
               />
             </Tabs.Item>
           ))}
         </Tabs>
 
       </div>
-
-
-
     </section>
-
-
   );
 }
