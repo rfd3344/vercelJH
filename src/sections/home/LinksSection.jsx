b819d84590770c0a7b8a737045a6e8c8b1ea9902
rfd3344w <rfd3344w@gmail.com>
2025-01-26 23:06:28 +1100

    Update LinksSection.jsx

--- src/sections/home/LinksSection.jsx
+++ src/sections/home/LinksSection.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import _ from 'lodash';
 
 import ButtonLink from './ButtonLink';
-
+import Typography from 'src/components/dataDisplay/typography';
 
 export default function LinksSection({ title = '', linkMap = {}, ...rest }) {
 
   return (
     <div className="mt-2" {...rest} >
-      <h4 className='font-bold'> {title} </h4>
+      <Typography variant='h3' > {title} </Typography>
       <div className='flex gap-3 flex-wrap' >
         {_.keys(linkMap).map((key) => (
           <ButtonLink key={key} text={key} to={linkMap[key]} />
