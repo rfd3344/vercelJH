3f5cbb8da9783e1d8fb24a4fb408a3ff9ae09b5e
Eric <rfd3344@gmail.com>
2025-01-27 12:43:15 +1100

    Update Note.tsx

--- src/sections/supabase/note/Note.tsx
+++ src/sections/supabase/note/Note.tsx
@@ -67,9 +67,14 @@ export default function Note() {
             </TabsContent>
           ))}
         </Tabs>
-        <Textarea
 
-        />
+        <h1>1111</h1>
+        <textarea id="w3review" name="w3review" rows="4" cols="50">
+          At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+        </textarea>
+        <input />
+        <input type="checkbox" />
+
       </div>
     </section>
   );
