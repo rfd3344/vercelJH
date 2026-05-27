8781257b46983480f58f81dd1307b8810cac7a62
eric <eli@conneqthealth.com>
2026-05-27 15:38:44 +1000

    chore: release v2.0.12

--- scripts/release.js
+++ scripts/release.js
@@ -11,7 +11,8 @@ function run(cmd) {
 try {
   run(`npm version patch --no-git-tag-version`);
   run('git add .');
-
+  const a = execSync('node -p "require(\'./package.json\').version"', { stdio: 'pipe' })
+  console.log('a:', a);
   const version = execSync('node -p "require(\'./package.json\').version"', { stdio: 'pipe' }).toString().trim();
   run(`git commit -m "chore: release v${version}"`);
   // run('git push');
